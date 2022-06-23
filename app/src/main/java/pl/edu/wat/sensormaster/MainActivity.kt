ed146d3227501603090fd2031b8c9aa8fa293841
waluwalu064 <waluwalu064@gmail.com>
2022-06-23 17:05:30 +0200

    Added comments in Polish - "documentation" of project :)

--- app/src/main/java/pl/edu/wat/sensormaster/MainActivity.kt
+++ app/src/main/java/pl/edu/wat/sensormaster/MainActivity.kt
@@ -13,20 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Zmienna obsługująca textView z xml:
         val textViewListOfSensors : TextView = findViewById(R.id.textView_listOfSensors)
         textViewListOfSensors.text = " "
 
+        //SensorManager do obsługi sensorów:
         val manager : SensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        //Inicjacja listy sensorów do wydrukowania ich nazw:
         val listOfSensors : List<Sensor> = manager.getSensorList(Sensor.TYPE_ALL)
 
+        //pętla do wydrukowania sensorów w print View:
         val listOfSensorsLength : Int = listOfSensors.size
-
-
         for(i in 0 until listOfSensorsLength) {
             textViewListOfSensors.text = textViewListOfSensors.text as String + "\n" + listOfSensors[i].name
         }
-
-
     }
 }
 
