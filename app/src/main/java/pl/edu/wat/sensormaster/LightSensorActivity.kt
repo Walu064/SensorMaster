ccf07cedfe02117e5ca119e873f65ad148048a32
waluwalu064 <waluwalu064@gmail.com>
2022-06-23 21:11:08 +0200

    Light sensor is working :)

--- app/src/main/java/pl/edu/wat/sensormaster/LightSensorActivity.kt
+++ app/src/main/java/pl/edu/wat/sensormaster/LightSensorActivity.kt
@@ -2,30 +2,49 @@ package pl.edu.wat.sensormaster
 
 import android.annotation.SuppressLint
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 
-class LightSensorActivity : AppCompatActivity() {
+class LightSensorActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var lightSensor : Sensor
+    private lateinit var manager : SensorManager
+    private lateinit var lightSensorTextView : TextView
+    private lateinit var infoTextView : TextView
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_light_sensor)
 
         //Inicjacja właściwych komponentów do wyświetlania wskazań sensora:
-        val lightSensorTextView : TextView = findViewById(R.id.textView_lightSensorData)
-        val infoTextView : TextView = findViewById(R.id.textView_info)
+        lightSensorTextView = findViewById(R.id.textView_lightSensorData)
+        infoTextView = findViewById(R.id.textView_info)
         infoTextView.text = "Wskazanie czujnika natężenia światła:"
 
         //Obiekt klasy sensor będący reprezentacją sensora natężenia światła:
-        val manager : SensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        val lightSensor : Sensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        manager  = getSystemService(SENSOR_SERVICE) as SensorManager
+        lightSensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        manager.registerListener(this, manager.getDefaultSensor(Sensor.TYPE_LIGHT), 0, null)
 
         //Obsługa błędów sensora:
         if(lightSensor == null) lightSensorTextView.text = "Błąd sensora!"
         else lightSensorTextView.text = "Sensor bangla"
+    }
 
-        //Funkcja do aktualizacji sensora:
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //Metoda wygenerowana automatycznie.
+    }
+
+    /**
+     Metoda nadpisująca wskazanie sensora:
+     */
+    override fun onSensorChanged(p0: SensorEvent?) {
+        lightSensorTextView.text = p0?.values?.get(0).toString()
     }
 }
